4e0db014c2d9ecaf175b2c89763b8caa692eca3f
b12howard <howard.luong87@gmail.com>
2024-03-16 23:27:42 -0700

    feat(app): remove files

--- script.js
+++
@@ -1,8 +0,0 @@
-// Function to open the Ko-Fi donation page
-function openDonatePage() {
-    window.open('https://ko-fi.com/YOUR_USERNAME', '_blank');
-}
-
-// Add click event listener to the donate button
-const donateBtn = document.querySelector('.donate-btn');
-donateBtn.addEventListener('click', openDonatePage);
